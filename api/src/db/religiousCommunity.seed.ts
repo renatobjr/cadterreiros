26a5f73ee3e0a47d83de80c4558cc90024b736d6
renatobjr <renato.bonfim.jr@cciao.org>
2025-05-29 09:26:21 -0300

    fix: refactor seed database

--- api/src/db/religiousCommunity.seed.ts
+++ api/src/db/religiousCommunity.seed.ts
@@ -3,31 +3,36 @@ import { initialData } from "@/api/db/data/initialData";
 import { getAddressFromLatLon } from "../configs/geocoder";
 
 const religiousCommunitySeed = async () => {
-  for (const community of initialData) {
-    try {
-      if (community.community_google_api_localization) {
-        const googleAddressFromLatLon = await getAddressFromLatLon(
-          community.community_google_api_localization.lat,
-          community.community_google_api_localization.long
-        );
+  const hasData = await ReligiousCommunity.countDocuments();
 
-        if (googleAddressFromLatLon) {
-          community.community_address = {
-            fullAddress: googleAddressFromLatLon.fullAddress,
-            street: googleAddressFromLatLon.street,
-            number: googleAddressFromLatLon.number,
-            neighborhood: googleAddressFromLatLon.neighborhood,
-            city: googleAddressFromLatLon.city,
-            state: googleAddressFromLatLon.state,
-            zipcode: googleAddressFromLatLon.zipcode,
-          };
+  if (hasData === 0) {
+    for (const community of initialData) {
+      try {
+        if (community.community_google_api_localization) {
+          const googleAddressFromLatLon = await getAddressFromLatLon(
+            community.community_google_api_localization.lat,
+            community.community_google_api_localization.long
+          );
+
+          if (googleAddressFromLatLon) {
+            community.community_address = {
+              fullAddress: googleAddressFromLatLon.fullAddress,
+              street: googleAddressFromLatLon.street,
+              number: googleAddressFromLatLon.number,
+              neighborhood: googleAddressFromLatLon.neighborhood,
+              city: googleAddressFromLatLon.city,
+              state: googleAddressFromLatLon.state,
+              zipcode: googleAddressFromLatLon.zipcode,
+            };
+          }
         }
-      }
 
-      await ReligiousCommunity.create(community);
-    } catch (error) {
-      throw new Error(error as string);
+        await ReligiousCommunity.create(community);
+      } catch (error) {
+        throw new Error(error as string);
+      }
     }
+    console.log("[✔] Database seed completed.");
   }
 };
 
