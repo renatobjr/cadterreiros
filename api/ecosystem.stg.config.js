7493d9a29709cbb13bf516ca2bbfb47a9d2df45a
renatobjr <renato.bonfim.jr@cciao.org>
2025-06-08 10:33:48 -0300

    fix: set mongo url

--- api/ecosystem.stg.config.js
+++ api/ecosystem.stg.config.js
@@ -9,7 +9,7 @@ module.exports = {
         API_PORT: "3000",
         NODE_BASE_URL: "/api/v1",
         GOOGLE_MAPS_API: "AIzaSyCww53qH4bTw9z2le42RZu0QFam20AiuyU",
-        MONGO_URL: "mongodb://localhost:27017",
+        MONGO_URL: "mongodb://127.0.0.1:27017",
         MONGO_INITDB_ROOT_USERNAME: "root",
         MONGO_INITDB_ROOT_PASSWORD: "password",
         MONGO_INITDB_DATABASE: "cadterreiros"
