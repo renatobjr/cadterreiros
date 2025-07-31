5c16daf01aa331f403ebbf6cd686a3def243db44
renatobjr <renato.bonfim.jr@cciao.org>
2025-07-30 22:38:48 -0300

    feat: include new prop on schema

--- api/src/schemas/ReligiousCommunity.ts
+++ api/src/schemas/ReligiousCommunity.ts
@@ -81,6 +81,7 @@ export interface IReligiousCommunity {
   leaderSufferedRacism: boolean;
   religiousSpaceMainPicture: string;
   religiousSpaceStatus: EReligiousSpaceStatus;
+  religiousSpaceLiturgicCalendar?: [];
   bio?: string | undefined;
   censusStep: ECensusStep;
   censusTaker: Types.ObjectId;
@@ -127,6 +128,7 @@ const ReligiousCommunitySchema = new Schema(
       enum: EReligiousSpaceStatus,
       default: EReligiousSpaceStatus.ACTIVE,
     },
+    religiousSpaceLiturgicCalendar: { type: Array, required: false },
     bio: { type: String, required: false },
     censusStep: {
       type: String,
